93235544329b6785286bdaa98757fca5c6e6bf8b
ASSASSINYM <darlingiloveu@sina.com>
2020-12-22 14:22:03 +0800

    Initial Commit

---
+++ iOSMacros/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  iOSMacros
+//
+//  Created by ASSASSIN on 2020/12/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSMacros/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  iOSMacros
+//
+//  Created by ASSASSIN on 2020/12/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
